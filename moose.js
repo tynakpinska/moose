58c63cbf8a7372667fd68038a57fbc30fcb3a8c7
tynakpinska <45312690+tynakpinska@users.noreply.github.com>
2019-03-23 22:45:07 +0100

    Add files via upload
    deleting console.log

--- moose.js
+++ moose.js
@@ -1,7 +1,5 @@
 const inputs = document.querySelectorAll('input');
 
-console.log(inputs);
-
 function changeStyle() {
     const suffix = this.dataset.sizing;
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
